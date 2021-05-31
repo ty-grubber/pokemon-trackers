6e11ce4099ab6ec22f66f7a0f2830aac6e97051b
Tyler Grubb <tgrubb@gasbuddy.com>
2021-05-31 17:40:54 -0600

    feat: add and use classnames package

--- pokemon-trackers/components/Layout/Layout.jsx
+++ pokemon-trackers/components/Layout/Layout.jsx
@@ -1,12 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from './layout.module.css'
-import utilStyles from '../../styles/utils.module.css'
-import Link from 'next/link'
+import classnames from 'classnames/bind';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from './layout.module.css';
+import utilStyles from '../../styles/utils.module.css';
+import Link from 'next/link';
+
+const cx = classnames.bind(styles);
+const ucx = classnames.bind(utilStyles);
 
 export default function Layout({ children, home }) {
   return (
-    <div className={styles.container}>
+    <div className={cx('container')}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta
@@ -14,20 +18,19 @@ export default function Layout({ children, home }) {
           content="Keep track of everything you need for your pokemon randomizers"
         />
         <meta name="og:title" content="Pokedex Trackers" />
-        <meta name="twitter:card" content="summary_large_image" />
       </Head>
-      <header className={styles.header}>
+      <header className={cx('header')}>
         {home ? (
           <>
             <Image
               priority
               src="/images/tiger-sigil.jpg"
-              className={utilStyles.borderCircle}
+              className={ucx('borderCircle')}
               height={144}
               width={144}
               alt="Tiger Image"
             />
-            <h1 className={utilStyles.heading2Xl}>Pokedex Trackers</h1>
+            <h1 className={ucx('heading2Xl')}>Pokedex Trackers</h1>
           </>
         ) : (
           <>
@@ -36,16 +39,16 @@ export default function Layout({ children, home }) {
                 <Image
                   priority
                   src="/images/tiger-sigil.jpg"
-                  className={utilStyles.borderCircle}
+                  className={ucx('borderCircle')}
                   height={108}
                   width={108}
                   alt="Tiger Image"
                 />
               </a>
             </Link>
-            <h2 className={utilStyles.headingLg}>
+            <h2 className={ucx('headingLg')}>
               <Link href="/">
-                <a className={utilStyles.colorInherit}>Home</a>
+                <a className={ucx('colorInherit')}>Home</a>
               </Link>
             </h2>
           </>
@@ -53,7 +56,7 @@ export default function Layout({ children, home }) {
       </header>
       <main>{children}</main>
       {!home && (
-        <div className={styles.backToHome}>
+        <div className={cx('backToHome')}>
           <Link href="/">
             <a>← Back to home</a>
           </Link>
--- pokemon-trackers/pages/index.js
+++ pokemon-trackers/pages/index.js
@@ -2,6 +2,9 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Layout from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import classnames from 'classnames/bind';
+
+const ucx = classnames.bind('utilStyles');
 
 export default function Home() {
   return (
@@ -9,7 +12,7 @@ export default function Home() {
       <Head>
         <title>Pokedex Trackers Home</title>
       </Head>
-      <section className={utilStyles.headingMd}>
+      <section className={ucx('headingMd')}>
         <h1>Pokedex Trackers</h1>
         <Link href="/pokedex/minesweeper">
           <a>Minesweeper</a>

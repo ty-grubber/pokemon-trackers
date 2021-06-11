d9ac84c3adf57957ae60602a1b8f9209e04d15e1
Tyler Grubb <tgrubb@gasbuddy.com>
2021-06-10 20:12:33 -0600

    fix: cleanup Minesweeper page

--- components/Pokedex/MinesweeperPage/PDMinesweeperPage.jsx
+++ components/Pokedex/MinesweeperPage/PDMinesweeperPage.jsx
@@ -1,10 +1,7 @@
 import classnames from 'classnames/bind';
 import Head from 'next/head';
-import Image from 'next/image';
 import React from 'react';
 import Layout from '../../Layout';
-import { GEN2 } from '../../../lib/constants/pokedex.js';
-import Grid from '../../Grid';
 import UnderConstruction from '../../UnderConstruction';
 import styles from './PDMinesweeperPage.module.css';
 
@@ -27,20 +24,6 @@ export default function Minesweeper() {
       </header>
       <section>
         <UnderConstruction />
-
-        <Grid className={cx('pokedexGrid')} columns={4}>
-          {GEN2.map(pokemon => (
-            <Grid.Cell key={pokemon.id}>
-              <Image
-                alt={`${pokemon.name}`}
-                src={`https://img.pokemondb.net/sprites/gold/normal/${pokemon.name.toLowerCase()}.png`}
-                title={`${pokemon.name}`}
-                width={25}
-                height={25}
-                />
-            </Grid.Cell>
-          ))}
-        </Grid>
       </section>
     </Layout>
   )
